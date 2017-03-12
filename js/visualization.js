2ab1fc18fe52d5f74c1becbe4f5955eb7e871edb
Dhruv Arora <dhruv.arora@asu.edu>
2017-03-11 17:09:55 -0700

    Search Feature to highlight airport based on name.

--- js/visualization.js
+++ js/visualization.js
@@ -3,17 +3,19 @@ var projection;
 var path;
 var height = 1000;
 var width = 1200;
-var airportByID = d3.map();
-var origin_mapping = d3.map();
+var objectArray = [];
+var airport_radius;
 
 function init() {
     svg = d3.select('body').append('svg')
         .attr('width', width)
         .attr('height', height);
 
+
     projection = d3.geoAlbers()
         .scale(1100)
         .translate([(width >> 1), (height >> 1)]);
+
 
     path = d3.geoPath()
         .projection(projection);
@@ -23,14 +25,6 @@ function init() {
         .defer(d3.json, './data/top200airports.json')
         .await(createMap);
 
-    queue()
-        .defer(d3.json, './data/top200airports.json')
-        .defer(d3.csv, './data/flights-airport.csv')
-        .await(buildConnectedGraph);
-
-    queue()
-        .defer(d3.json, './data/top200airports.json')
-        .await(configureSearch);
 }
 function createMap(error, states, airport_data) {
 
@@ -46,7 +40,7 @@ function createMap(error, states, airport_data) {
     var min_passenger_value = Math.min.apply(Math, passenger_traffic);
     var max_passenger_value = Math.max.apply(Math, passenger_traffic);
 
-    var airport_radius = d3.scalePow()
+    airport_radius = d3.scalePow()
         .domain([min_passenger_value, max_passenger_value])
         .range([10, 30]);
 
@@ -66,6 +60,8 @@ function createMap(error, states, airport_data) {
         }))
         .attr('class', 'cities')
         .on("mousemove", function (d) {
+
+
             d3.select(this).style("fill-opacity", 1);
             var mouse = d3.mouse(svg.node()).map(function (value) {
                 return parseInt(value);
@@ -81,60 +77,90 @@ function createMap(error, states, airport_data) {
             tooltip.classed('hidden', true);
         });
 
-}
-
-function buildConnectedGraph(error, airports, source_destination_list) {
-
-    var airport_length = airports.features.length;
-    for (var index = 0; index < airport_length; index++) {
-        var pop = airports.features[index].properties.LOCID;
-        airportByID.set(String(pop), airports.features[index].geometry.coordinates);
-    }
-
-    source_destination_list.forEach(function (flight) {
-        if (airportByID.has(flight.origin) && airportByID.has(flight.destination)) {
-            if (origin_mapping.has(flight.origin)) {
-                source_coordinates = [];
-                source_coordinates = origin_mapping.get(flight.origin);
-                source_coordinates.push(airportByID.get(flight.destination));
-                origin_mapping.set(flight.origin, source_coordinates);
-            } else {
-                source_coordinates = [];
-                source_coordinates.push(airportByID.get(flight.destination));
-                origin_mapping.set(flight.origin, source_coordinates);
-            }
-        }
-    });
-}
-
-function configureSearch(error, airports_list) {
-
-    var airport_length = airports_list.features.length;
     var airport_name_list = [];
     var airport_code_list = [];
     var div = document.querySelector("#filterCriteria"),
         frag = document.createDocumentFragment(),
         select = document.createElement("select");
     for (var index = 0; index < airport_length; index++) {
-        var airport_name = airports_list.features[index].properties.NAME;
+        var airport_name = airport_data.features[index].properties.NAME;
         airport_name_list.push({ID: String(airport_name)});
-        var airport_code = airports_list.features[index].properties.LOCID;
+        var airport_code = airport_data.features[index].properties.LOCID;
         airport_code_list.push({ID: String(airport_code)});
     }
-    filterResult();
+
 
     d3.select('#filterCriteria').on('keyup', filterResult);
 
     function filterResult() {
 
-        var filterText = d3.select('#filterCriteria').property('value');
-        var result;
-        var filteredAirportName = airport_name_list;
-        var filteredAirportCode = airport_code_list;
-        if (filterText !== "") {
-            filteredAirportName = airport_name_list.filter(function (airport) {
+        svg.selectAll('.highlighted_cities').remove();
+
+        svg.selectAll('.cities')
+            .data(airport_data.features)
+            .enter()
+            .append('path')
+            .attr("d", path.pointRadius(function (airport) {
+
+                return airport_radius(airport.properties.TOT_ENP);
+            }))
+            .attr('class', 'cities')
+            ;
+
+        var filteredAirportName_1 = [];
+        var filterText = document.getElementById('filterCriteria').value;
+
+        if (filterText != null && filterText != "") {
+            filteredAirportName_1.push(airport_name_list.filter(function (airport) {
                 return (airport.ID.toLowerCase().indexOf(filterText.toLowerCase()) === 0);
-            });
+            }));
         }
+
+
+        if (filteredAirportName_1.length != 0){
+            var objectArray=[];
+            for (var i = 0; i < filteredAirportName_1[0].length; i++) {
+                    for (var j = 0; j < airport_data.features.length; j++) {
+
+                        if (airport_data.features[j].properties.NAME == filteredAirportName_1[0][i].ID) {
+                            objectArray.push(airport_data.features[j]);
+                        }
+
+                    }
+                }
+            svg.selectAll('.highlighted_cities')
+                .data(objectArray)
+                .enter()
+                .append('path')
+                .attr("d", path.pointRadius(function (airport) {
+
+                    return airport_radius(airport.properties.TOT_ENP);
+                }))
+                .attr('class', 'highlighted_cities')
+                .on("mousemove", function (d) {
+
+
+
+                var mouse = d3.mouse(svg.node()).map(function (value) {
+                    return parseInt(value);
+                });
+                tooltip.classed('hidden', false)
+                    .attr('style', 'left:' + (900) +
+                        'px; top:' + (200) + 'px;right:'+(100)+'px;')
+                    .html(d.properties.NAME);
+
+            })
+                .on('mouseout', function () {
+
+                    tooltip.classed('hidden', true);
+                });
+
+        }
+
+
+
     }
+
 }
+
+
