b806a84414bd21e126f0174f2099dc10aaf11e1f
Deepankit <deepankitakhil@gmail.com>
2017-04-09 18:21:44 -0700

    Minor refactoring

--- js/bee_swarm.js
+++ js/bee_swarm.js
@@ -1,14 +1,11 @@
-/**
- * Created by dhruvarora on 4/8/17.
- */
-function draw_bee_swarm() {
+function display_bee_swarm() {
     var margin = {top: 40, right: 40, bottom: 40, left: 40},
         width = 845,
         height = 100;
 
     var svg = d3.select("#mean_data").append('svg')
-        .attr('width',width)
-        .attr('height',height);
+        .attr('width', width)
+        .attr('height', height);
 
 
     var formatValue = d3.format(",d");
@@ -17,15 +14,18 @@ function draw_bee_swarm() {
         .range([0, width]);
 
     var g = svg.append("g");
-        //.attr("transform", "translate(" + margin.left + ")");
 
-    d3.csv("./data/flare.csv", type, function(error, data) {
+    d3.csv("./data/flare.csv", type, function (error, data) {
         if (error) throw error;
 
-        x.domain(d3.extent(data, function(d) { return d.value; }));
+        x.domain(d3.extent(data, function (d) {
+            return d.value;
+        }));
 
         var simulation = d3.forceSimulation(data)
-            .force("x", d3.forceX(function(d) { return x(d.value); }).strength(1))
+            .force("x", d3.forceX(function (d) {
+                return x(d.value);
+            }).strength(1))
             .force("y", d3.forceY(height / 2))
             .force("collide", d3.forceCollide(4))
             .stop();
@@ -36,9 +36,6 @@ function draw_bee_swarm() {
         g.append("g")
             .attr("class", "axis axis--x")
             .attr("transform", "translate(0," + 80 + ")")
-
-            //.attr("d", "M" + width + ",0L" + (width + 20) + ",0L" + (width + 20) + ",-5L" + (width + 30) + ",0L"  + (width + 20) + ",5L" + (width + 20) + ",0L")
-
             .call(d3.axisBottom(x).ticks(20, ".0s"));
 
         var cell = g.append("g")
@@ -46,20 +43,32 @@ function draw_bee_swarm() {
             .selectAll("g")
             .data(d3.voronoi()
                 .extent([[-margin.left, -margin.top], [width + margin.right, height + margin.top]])
-                .x(function(d) { return d.x; })
-                .y(function(d) { return d.y; })
+                .x(function (d) {
+                    return d.x;
+                })
+                .y(function (d) {
+                    return d.y;
+                })
                 .polygons(data)).enter().append("g");
 
         cell.append("circle")
             .attr("r", 3)
-            .attr("cx", function(d) { return d.data.x; })
-            .attr("cy", function(d) { return d.data.y; });
+            .attr("cx", function (d) {
+                return d.data.x;
+            })
+            .attr("cy", function (d) {
+                return d.data.y;
+            });
 
         cell.append("path")
-            .attr("d", function(d) { return "M" + d.join("L") + "Z"; });
+            .attr("d", function (d) {
+                return "M" + d.join("L") + "Z";
+            });
 
         cell.append("title")
-            .text(function(d) { return d.data.id + "\n" + formatValue(d.data.value); });
+            .text(function (d) {
+                return d.data.id + "\n" + formatValue(d.data.value);
+            });
     });
 
     function type(d) {
